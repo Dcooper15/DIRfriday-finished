2992bd69c8ae72e92ac54fa08a740db02270fd6d
Dylan Cooper <56250259+Dcooper15@users.noreply.github.com>
2020-12-04 10:18:22 -0500

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -30,8 +30,3 @@ async function getRandomDogs() {
 
 goButton.addEventListener('click', getRandomDogs);
 
-
-
-//     const response = await fetch(url);
-//   const json = await response.json();
-//   console.log(json);
